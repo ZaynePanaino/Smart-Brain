27e9e1e3e16efd7504440c79e98d08382212fcf8
Zayne Panaino <Zayne.panaino@gmail.com>
2020-06-08 17:48:06 +0200

    Update ImageLinkForm.js

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f3'>
-				{'This powerful brain will detect faces in your pictures. Give it a try!'}
+				{'This powerful brain will detect faces in your pictures. Paste a link below to let The Brain work its magic!'}
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
